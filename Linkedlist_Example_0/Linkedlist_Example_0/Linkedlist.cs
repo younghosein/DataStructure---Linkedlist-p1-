d4af0cc96ceeff1e65983343da30a98bad102f08
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-21 11:46:08 +0330

    Add files via upload

---
+++ Linkedlist_Example_0/Linkedlist_Example_0/Linkedlist.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Linkedlist_Example_0
+{
+    class Linkedlist
+    {
+        public Node head;
+
+        public void print()
+        {
+            Node n = head;
+            while (n != null)
+            {
+                Console.Write(n.data+" . ");
+                n = n.next;
+            }
+        }
+        //-
+        public void push(int nd)
+        {
+            Node nn = new Node(nd);
+
+            nn.next = head;
+            head = nn;
+        }
+
+        public void AddAfter(int nd,Node bn)
+        {
+            if (bn == null)
+            {
+                Console.WriteLine("vojodesho ndre ddsh!");
+                return;
+            }
+            Node nn = new Node(nd);
+
+            nn.next = bn.next;
+
+            bn.next = nn;
+        }
+
+        public void append(int nd)
+        {
+            Node nn = new Node(nd);
+
+            if (head == null)
+            {
+                head = new Node(nd);
+                return;
+            }
+
+            nn.next = null;
+
+            Node last = head;
+
+            while (last.next != null)
+            {
+                last = last.next;
+            }
+
+            last.next = nn;
+
+        }
+        public void deln(int d)
+        {
+            Node n = head;
+            Node t = null;
+
+            //if (n != null && n.data == d)
+            {
+                head = n.next;
+                return;
+            }
+            while (n != null && n.data != d)
+            {
+                t = n;
+                n = n.next;
+            }
+            //if (n == null)
+            {
+                return;
+            }
+            t.next = n.next;
+        }
+
+        public void delp(int p)
+        {
+            if (head == null)
+            {
+                return;
+            }
+            Node n = head;
+            if (p == 0)
+            {
+                head = n.next;
+                return;
+            }
+            for(int i = 0; n != null && i < p - 1; i++)
+            {
+                n = n.next;
+            }
+            if (n == null || n.next == null)
+            {
+                return;
+            }
+            Node nxt = n.next.next;
+            n.next = nxt;
+        }
+    }
+}
---
+++ Linkedlist_Example_0/Linkedlist_Example_0/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlist_Example_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Developed By Hosein Mohamadpor");
+            Console.ResetColor();
+
+            Linkedlist L = new Linkedlist();
+
+            L.head = new Node(38);//->data1
+            Node sec= new Node(-56);//->data2
+            Node thi= new Node(99);//->data3
+            L.head.next = sec;//->pointer1
+            sec.next = thi;//->pointer2
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Linkedlist Is :");
+            L.print();
+            Console.ResetColor();
+
+            Console.WriteLine("\nEnter The Number To Be Added :");
+            int nump = int.Parse(Console.ReadLine());
+            L.push(nump);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nNow Linkedlist Is ( After Adding At First ) :");
+            L.print();
+            Console.ResetColor();
+            Console.WriteLine(" ");
+
+            L.AddAfter(70, L.head.next.next);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\nNow Linkedlist Is ( After Adding At list [ 3 ] ) :");
+            L.print();
+            Console.ResetColor();
+            Console.WriteLine(" ");
+
+            Console.WriteLine("\nEnter The Number To Be Added :");
+            int nume = int.Parse(Console.ReadLine());
+            L.append(nume);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\nNow Linkedlist Is ( After Adding At End ) :");
+            L.print();
+            Console.ResetColor();
+
+            Console.WriteLine("\nEnter The Number To Be Deleted :");
+            int numdn = int.Parse(Console.ReadLine());
+            L.deln(numdn);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nNow Linkedlist Is ( After Deleting Input Number ) :");
+            L.print();
+            Console.ResetColor();
+
+            Console.WriteLine("\nEnter The Number To Be Deleted :");
+            int numpn = int.Parse(Console.ReadLine());
+            L.delp(numpn);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nNow Linkedlist Is ( After Deleting Input Position ) :");
+            L.print();
+            Console.ResetColor();
+
+            Console.ReadKey();
+
+        }
+    }
+}
